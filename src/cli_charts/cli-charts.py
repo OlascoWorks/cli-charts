ac50b76e425980072fa38757f297006854d66395
olascoworks <samuelayodeji2478@gmail.com>
2024-03-21 13:08:18 +0100

    Fixing `draw bar`

--- src/cli_charts/cli-charts.py
+++ src/cli_charts/cli-charts.py
@@ -8,17 +8,17 @@ class CHART:
         for value in values:
             if type(value) != list:  raise TypeError("Values must be a list of lists")
 
-        for value in values:
             for iter, coord in enumerate(value):
-                # if type(coord) != int or type(coord) != float:  raise TypeError("coordinates must be a list of lists of numbers")
+                if type(coord) != int and type(coord) != float:  raise TypeError("coordinates must be a list of lists of numbers")
                 value[iter] = round(coord)
+                if self.granular == True:  value[iter] = value[iter] +1 if value[iter] % 2 != 0 else value[iter]
 
         yMax = max([cord[1] for cord in values])
         xMax = max([cord[0] for cord in values])
         yTop = math.ceil(yMax / 10) * 10
         xTop = math.ceil(xMax / 10) * 10
         labelWidth = len(str(yMax)) +1
-        # coordinates = {cord[1][:cord[1]] for cord in values}
+        step = 2 if self.granular == True else 1
         coordinates = {}
         graph = f"""{' ' *labelWidth}.\n{' ' *labelWidth}.\n{' ' *labelWidth}.\n"""
 
@@ -30,13 +30,11 @@ class CHART:
 
                 coordinates[value[1]] = sorted(coordinates[value[1]])
 
-        for iter in range(yTop+1):
+        for iter in range(0, yTop+1, step):
             pos = yTop - iter
             if pos % 10 == 0 and pos != 0:  graph += str(pos) + " +"
-            elif pos == 0:  graph += ' ' + str(pos) + " +"
-            else:  graph += f"{' ' *labelWidth}."
-
-            if pos == 0:
+            elif pos == 0:
+                graph += ' ' + str(pos) + " +"
                 for n in range(int(xTop/10)):
                     if n*10 !=( xTop -10):  graph += "---------+"
                     else:
@@ -44,14 +42,15 @@ class CHART:
                         for n in range(int(xTop/10)):
                             if n == 0:  graph += f"{' ' *labelWidth}         {(n*10) +10}"
                             else:  graph += f"        {(n*10) +10}"
+            else:  graph += f"{' ' *labelWidth}."
 
             if pos in coordinates.keys():
                 if type(coordinates[pos]) == list:
                     coords = coordinates[pos]
                     plot = ''
                     for k, n in enumerate(coords):
-                        if k == 0:  plot += " "*(coordinates[pos][k] - 1) + "*"
-                        else:  plot += " "*((coordinates[pos][k] -len(plot)) -1) + "*"
+                        if k == 0:  plot += " "*(coords[k] - 1) + "*"
+                        else:  plot += " "*((coords[k] -len(plot)) -1) + "*"
                         if k == len(coords) -1:  plot += "\n"
 
                     graph += plot
@@ -62,6 +61,7 @@ class CHART:
 
         print(graph)
 
+    # def draw_bar(elf, values:list, barWidth=5):
 
     def __str__(self) -> str:
         return ""
@@ -70,5 +70,7 @@ class CHART:
         return self.__str__
     
 
-chart = CHART()
-chart.plot_graph([[15.3,4.9], [21.3,6.7], [26.4,8.4], [29.4,9.4], [34,10.8]])
+chart1 = CHART()
+chart2 = CHART(True)
+chart1.plot_graph([[15.3,4.9], [21.3,6.7], [26.4,8.4], [29.4,9.4], [34,10.8]])
+chart2.plot_graph([[15.3,4.9], [21.3,6.7], [26.4,8.4], [29.4,9.4], [34,10.8]])
